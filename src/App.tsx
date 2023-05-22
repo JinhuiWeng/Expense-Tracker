21f37904643262dd3a8778d54d63f3cc3c1d6c68
Jason Weng <jasonweng95@gmail.com>
2023-05-23 01:38:28 +1000

    Added table sorting

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-// import Alert from "./components/Alert";
-// import ListGroup from "./components/ListGroup";
-// import Buttons from "./components/Buttons";
-// import Like from "./components/Like/Like";
-// import NavBar from "./components/NavBar";
-// import Cart from "./components/Cart";
-// import ExpandableText from "./components/ExpandableText";
 import { useEffect, useState } from "react";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseForm from "./components/ExpenseForm";
 import categories from "./components/categories";
 
-// const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-// const handleSelectItem = (item: string) => {
-//   console.log(item);
-// };
+
 
 function App() {
-  // const [showStatus, setShowStatus] = useState(false);
-  // const [cartItems, setCartItems] = useState(["Products1", "Product2"]);
-  // const handleClose = () => setShowStatus(false);
-  // const [isExpanded, setIsExpanded] = useState(false);
-
 
   const [expenses, setExpenses] = useState(
     localStorage.getItem("expenses") != null
@@ -32,8 +17,9 @@ function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
 
   const handleDelte = (id: number) => {
-    setExpenses(expenses.filter((expense: any) => expense.id !== id));
-    localStorage.setItem("expenses", JSON.stringify(expenses));
+    const updatedExpenses = expenses.filter((expense: any) => expense.id !== id);
+    setExpenses(updatedExpenses);
+    localStorage.setItem("expenses", JSON.stringify(updatedExpenses));
   };
 
   const handleDelteAll = () => {
@@ -66,45 +52,6 @@ function App() {
         onDelete={handleDelte}
         onDeleteAll={handleDelteAll}
       />
-      {/* <ExpandableText isExpanded={isExpanded} onExpand={() => setIsExpanded(true)} onClose={() => setIsExpanded(false)}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat iure
-        accusamus ipsa dolore provident possimus at culpa commodi aliquid alias
-        molestias hic laudantium dolores ad, illum quaerat perspiciatis,
-        eligendi pariatur, similique asperiores delectus mollitia perferendis
-        blanditiis! Quisquam nulla dolorum ducimus cumque, deleniti laudantium
-        nemo alias eius dolorem mollitia nesciunt animi ipsam aliquam, incidunt
-        libero blanditiis voluptate? Sint ducimus nobis molestiae officia, iusto
-        neque porro quisquam fuga dolorum minima veniam sequi, corporis sapiente
-        omnis voluptate ipsa tenetur voluptates assumenda tempora quos, illum
-        obcaecati? Nihil optio sapiente sed magnam, labore blanditiis inventore
-        ipsum minima aliquid minus. Eos soluta optio laboriosam sint, aperiam
-        quas similique maxime ducimus adipisci dolore qui aut perspiciatis
-        molestias error esse dolorum excepturi consequuntur provident, voluptate
-        distinctio reiciendis! Quaerat nobis in, atque id eos dicta nihil
-        impedit voluptates assumenda porro iste voluptas aut sint provident enim
-        at fugit tempora ducimus nostrum voluptate suscipit ab architecto vitae?
-        Vitae fugiat iure cumque earum blanditiis repellat qui aspernatur iste
-        pariatur debitis cupiditate quas officia ratione, dolores beatae,
-        impedit, iusto modi ex commodi nostrum minus reiciendis sit! Quae ipsa
-        deserunt harum reiciendis amet nobis laborum quis numquam assumenda
-        earum adipisci illo, incidunt a officia corporis facilis tempora
-        architecto excepturi itaque rem quo molestias!
-      </ExpandableText> */}
-      {/* <game games={games} onUpdate={setGame({...games, player: {...games.player, name: "Rob"}})} /> */}
-
-      {/* <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} /> */}
-
-      {/* <Like onClick={() => console.log('clicked')} /> */}
-
-      {/* {showStatus && <Alert onClick = {handleClose}>My Alert</Alert>}
-      <Buttons onClick={() => setShowStatus(true)}>My Button</Buttons> */}
-
-      {/* <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      /> */}
     </div>
   );
 }
--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { MouseEventHandler, useCallback, useState } from "react";
+import { AiFillCaretUp, AiFillCaretDown } from "react-icons/ai";
 import moment from "moment";
 
 interface Expense {
@@ -15,55 +16,150 @@ interface Props {
   onDeleteAll: () => void;
 }
 
+type SortKeys = keyof Expense;
+type SortOrder = "ascn" | "desc";
+
+function SortExpense({
+  tableData,
+  sortKey,
+  reverse,
+}: {
+  tableData: Expense[];
+  sortKey: SortKeys;
+  reverse: boolean;
+}) {
+  if (!sortKey) return tableData;
+  const sortedTableData = tableData.sort((a, b) => {
+    return a[sortKey] > b[sortKey] ? 1 : -1;
+  });
+
+  if (reverse) {
+    return sortedTableData.reverse();
+  }
+  return sortedTableData;
+}
+
+function SortButton({
+  sortOrder,
+  columnKey,
+  sortKey,
+  onClick,
+}: {
+  sortOrder: SortOrder;
+  columnKey: SortKeys;
+  sortKey: SortKeys;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}) {
+  return sortKey === columnKey && sortOrder === "desc" ? (
+    <AiFillCaretDown onClick={onClick}></AiFillCaretDown>
+  ) : (
+    <AiFillCaretUp onClick={onClick}></AiFillCaretUp>
+  );
+}
+
 const ExpenseList = ({ expenses, onDelete, onDeleteAll }: Props) => {
+  const [sortKey, setSortKey] = useState<SortKeys>("description");
+  const [sortOrder, setSortOrder] = useState<SortOrder>("ascn");
+
+  const headers: { key: SortKeys; label: string }[] = [
+    {
+      key: "description",
+      label: "Description",
+    },
+    {
+      key: "amount",
+      label: "Amount",
+    },
+    {
+      key: "category",
+      label: "Category",
+    },
+    {
+      key: "date",
+      label: "Date",
+    },
+  ];
+
+  function changeSort(key: SortKeys) {
+    setSortOrder(sortOrder === "ascn" ? "desc" : "ascn");
+    setSortKey(key);
+  }
+
+  const sortedExpenses = useCallback(
+    () =>
+      SortExpense({
+        tableData: expenses,
+        sortKey,
+        reverse: sortOrder === "desc",
+      }),
+    [expenses, sortKey, sortOrder]
+  );
+
   if (expenses.length === 0) return <p>No expenses</p>;
 
   return (
-    <table className="table table-striped table-bordered">
-      <thead>
-        <tr>
-          <th scope="col">Description</th>
-          <th scope="col">Amount</th>
-          <th scope="col">Date</th>
-          <th scope="col">Category</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {expenses.map((expense) => (
-          <tr key={expense.id}>
-            <td>{expense.description}</td>
-            <td>${expense.amount.toFixed(2)}</td>
-            <td>{expense.category}</td>
-            <td>{moment(expense.date).format("YYYY-MM-DD")}</td>
+    <>
+      <p className="mt-3">Showing {expenses.length} Expenses</p>
+      <table className="table table-striped table-bordered ">
+        <thead>
+          <tr>
+            {headers.map((row) => (
+              <td key={row.key}>
+                {row.label}
+                <SortButton
+                  columnKey={row.key}
+                  onClick={() => changeSort(row.key)}
+                  {...{
+                    sortKey,
+                    sortOrder,
+                  }}
+                />
+              </td>
+            ))}
+            <td />
+          </tr>
+        </thead>
+        <tbody>
+          {sortedExpenses().map((expense) => (
+            <tr key={expense.id}>
+              <td>{expense.description}</td>
+              <td>${expense.amount.toFixed(2)}</td>
+              <td>{expense.category}</td>
+              <td>{moment(expense.date).format("YYYY-MM-DD")}</td>
+              <td>
+                {/* <td className="col-sm-4">{expense.description}</td>
+              <td className="col-sm-2">${expense.amount.toFixed(2)}</td>
+              <td className="col-sm-2">{expense.category}</td>
+              <td className="col-sm-2">{moment(expense.date).format("YYYY-MM-DD")}</td>
+              <td className="col-sm-1"> */}
+                <button
+                  className="btn btn-outline-danger delete-btn"
+                  onClick={() => onDelete(expense.id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>Total</td>
             <td>
-              <button
-                className="btn btn-outline-danger"
-                onClick={() => onDelete(expense.id)}
-              >
-                Delete
+              $
+              {sortedExpenses()
+                .reduce((acc, expense) => expense.amount + acc, 0)
+                .toFixed(2)}
+            </td>
+            <td colSpan={3}>
+              <button className="btn btn-danger" onClick={() => onDeleteAll()}>
+                Delete All Expenses!
               </button>
             </td>
           </tr>
-        ))}
-      </tbody>
-      <tfoot>
-        <tr>
-          <td>Total</td>
-          <td>
-            $
-            {expenses
-              .reduce((acc, expense) => expense.amount + acc, 0)
-              .toFixed(2)}
-          </td>
-          <td colSpan={3}>
-            <button className="btn btn-danger" onClick={() => onDeleteAll()}>
-              Delete All Expenses!
-            </button>
-          </td>
-        </tr>
-      </tfoot>
-    </table>
+        </tfoot>
+      </table>
+    </>
   );
 };
 
